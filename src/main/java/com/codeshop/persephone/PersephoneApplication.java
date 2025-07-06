458df46188fb055d2a11ae2531a47c1c7bfb190b
Karol Latos <pl.charlessummers@gmail.com>
2025-07-06 13:00:30 +0200

    Create empty Spring Boot project

---
+++ src/main/java/com/codeshop/persephone/PersephoneApplication.java
@@ -0,0 +1,13 @@
+package com.codeshop.persephone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersephoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersephoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeshop/persephone/PersephoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeshop.persephone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersephoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

880357e93b24ccfd16cdce386bd62bbf5d730184
Karol Latos <pl.charlessummers@gmail.com>
2025-07-10 00:10:46 +0200

    Utilize environment variables

--- src/main/java/com/codeshop/persephone/security/CorsConfig.java
+++ src/main/java/com/codeshop/persephone/security/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.codeshop.persephone.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,7 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-    private static final String[] ALLOWED_ORIGINS = { "https://persephone.run", "https://api.persephone.run" };
+
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -15,7 +18,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@SuppressWarnings("NullableProblems") CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins(ALLOWED_ORIGINS)
+                    .allowedOrigins(allowedOrigins)
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                     .allowedHeaders("*")
                     .exposedHeaders("ETag", "Content-Length", "Accept-Ranges", "Content-Range")

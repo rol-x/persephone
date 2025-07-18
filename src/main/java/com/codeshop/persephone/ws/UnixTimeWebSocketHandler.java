effe9fd294de877314c1d2f3184eb929fa928202
Karol Latos <pl.charlessummers@gmail.com>
2025-07-18 11:25:08 +0200

    Broadcast via WebSockets every 0.1 seconds

--- src/main/java/com/codeshop/persephone/ws/UnixTimeWebSocketHandler.java
+++ src/main/java/com/codeshop/persephone/ws/UnixTimeWebSocketHandler.java
@@ -14,7 +14,7 @@ public class UnixTimeWebSocketHandler extends BaseTextWebSocketHandler {
 
     @Override
     protected void startScheduledTask() {
-        scheduledTask = schedule(this::broadcastUnixTime, 500);
+        scheduledTask = schedule(this::broadcastUnixTime, 100);
     }
 
     private void broadcastUnixTime() {

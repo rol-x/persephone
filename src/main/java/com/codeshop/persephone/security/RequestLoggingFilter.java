bc7067f8e19f48f9042da9cf854d61a5a1a1e7e1
Karol Latos <pl.charlessummers@gmail.com>
2025-07-19 10:13:44 +0200

    Annotate HTTP filter arguments with @NonNull

--- src/main/java/com/codeshop/persephone/security/RequestLoggingFilter.java
+++ src/main/java/com/codeshop/persephone/security/RequestLoggingFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
@@ -17,8 +18,8 @@ public class RequestLoggingFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
         throws ServletException, IOException {
 
         String uri = request.getRequestURI();

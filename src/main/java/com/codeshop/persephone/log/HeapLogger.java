8131de08979c59929138a9d857d3212ced8c6511
Karol Latos <pl.charlessummers@gmail.com>
2025-07-19 13:57:20 +0200

    Log heap memory conditionally (on significant change)

--- src/main/java/com/codeshop/persephone/log/HeapLogger.java
+++ src/main/java/com/codeshop/persephone/log/HeapLogger.java
@@ -6,14 +6,57 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class HeapLogger {
+public class HeapLogger { // why so serious?
+    private long lastUsed;
+    private long lastCommitted;
     private final Runtime r = Runtime.getRuntime();
+    private final long maxMemoryInMB = getMaxMemoryInMB();
 
-    @Scheduled(fixedRate = 30_000)
-    public void logHeap() {
-        long used       = (r.totalMemory() - r.freeMemory()) >> 20;
-        long committed  = r.totalMemory() >> 20;
-        long max        = r.maxMemory() >> 20;
-        log.info("Heap(MB) used={} committed={} max={}", used, committed, max);
+    public HeapLogger() {
+        lastUsed        = getUsedMemoryInMB();
+        lastCommitted   = getCommittedMemoryInMB();
+    }
+
+    @Scheduled(fixedRate = 10_000)
+    public void logHeapChange() {
+        long used       = getUsedMemoryInMB();
+        long committed  = getCommittedMemoryInMB();
+
+        var logMessage = buildLog(used, committed);
+        if (committed != lastCommitted || used < lastUsed) {
+            log.info(logMessage);
+        }
+        lastUsed = used;
+        lastCommitted = committed;
+    }
+
+    private String buildLog(long used, long committed) {
+        StringBuilder sb = new StringBuilder(50);
+        sb.append("Heap(MB) used=");
+        if (used < lastUsed) {
+            sb.append(lastUsed).append("->").append(used);
+        } else {
+            sb.append(used);
+        }
+        sb.append(" committed=");
+        if (committed != lastCommitted) {
+            sb.append(lastCommitted).append("->").append(committed);
+        } else {
+            sb.append(committed);
+        }
+        sb.append(" max=").append(maxMemoryInMB);
+        return sb.toString();
+    }
+
+    private long getUsedMemoryInMB() {
+        return (r.totalMemory() - r.freeMemory()) >> 20;
+    }
+
+    private long getCommittedMemoryInMB() {
+        return r.totalMemory() >> 20;
+    }
+
+    private long getMaxMemoryInMB() {
+        return r.maxMemory() >> 20;
     }
 }
